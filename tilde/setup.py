99945dfc0c7ea72cbbf30fc2624aef145a98ec39
Chris Schneider <110739072+ChrisKnorri@users.noreply.github.com>
2024-11-08 15:09:10 +0000

    Added new src folder for succesful build
    Now two robots are instantiated. Movement is not yet included and needs to be addressed as the next step.

---
+++ tilde/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'tilde'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    # Packages to export
+    packages=[package_name],
+    # Files we want to install, specifically launch files
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/launch/", glob("launch/*launch*")),
+        ('share/' + package_name + "/rviz/", glob("rviz/*")),
+        ('share/' + package_name + "/world/" , glob('world/*')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='chris',
+    maintainer_email='christopher.schneider@gmx.net',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    # Declare the enry point for your python scripts
+    entry_points={
+        'console_scripts': [
+            'tilde = tilde.__init__:main'
+        ],
+    },
+)
